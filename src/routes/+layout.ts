1a5bbdbbffc148dd73dd35d217be364657e5a4c5
Tim <tima@nopublic.ru>
2024-03-29 06:21:36 +0300

    Add temporary redirect confirmation

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,6 +3,8 @@ export const ssr = false
 export function load() {
 	const isOpenedFromTelegram = Boolean(window.Telegram.WebView.initParams.tgWebAppThemeParams)
 	if(!isOpenedFromTelegram) {
-		window.location.href = 'https://t.me/campfire_game_bot/app'
+		if(window.confirm('Do you want to open Campfire Telegram Mini App?')) {
+			window.location.href = 'https://t.me/campfire_game_bot/app'
+		}
 	}
 }
