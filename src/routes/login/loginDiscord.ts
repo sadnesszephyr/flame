9acd535564d04c6eb248b538f7c9a3193d455db4
Tim Nikolsky <tim@timnik.me>
2025-01-17 06:11:06 +0300

    fix: discord oauth url

--- src/routes/login/loginDiscord.ts
+++ src/routes/login/loginDiscord.ts
@@ -1,4 +1,10 @@
+import { PUBLIC_BASE_URL, PUBLIC_DISCORD_CLIENT_ID } from '$env/static/public';
+
 export function loginDiscord() {
 	window.location.href =
-		'https://discord.com/oauth2/authorize?client_id=659814478119960587&response_type=code&redirect_uri=https%3A%2F%2Fcampfire-dev.timnik.me%2Flogin%3Fmethod%3Ddiscord&scope=identify';
+		'https://discord.com/oauth2/authorize?client_id=' +
+		PUBLIC_DISCORD_CLIENT_ID +
+		'&response_type=code&redirect_uri=' +
+		encodeURIComponent(PUBLIC_BASE_URL) +
+		'%2Flogin%3Fmethod%3Ddiscord&scope=identify';
 }
